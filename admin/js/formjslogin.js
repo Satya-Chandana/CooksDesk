b11203df21222ab93397358fc319d8fdf468803b
Satya-Chandana <msatyachandana@gmail.com>
2021-10-27 01:17:11 +0530

    form js for login

---
+++ admin/js/formjslogin.js
@@ -0,0 +1,79 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+
+/*form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+    if (checkInputs) {
+        alert("Form submitted succesfully");
+    }
+
+	checkInputs();
+    
+});*/
+
+function checkInputs() {
+	// trim to remove the whitespaces
+
+	const emailValue = email.value.trim();    
+	const passwordValue = password.value.trim();
+	
+    
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+		return false;
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+		return false;
+	} else {
+		setSuccessFor(email);
+	}
+
+
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+		return false;
+	} else {
+		setSuccessFor(password);
+	}
+	
+
+	// alert("Login Sucessful");
+    return true;
+}
+
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function isnum(num) {
+	return /^(([0-9]{5}-[0-9]{5}))$/.test(num);
+}
+
+
+
+
+
+
+
+
+
+
+
